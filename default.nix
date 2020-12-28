3eaea0d7f4532cace6a0e8df390930a94e2ec786
kczulko <karol.czulkowski@protonmail.com>
2020-12-28 13:10:07 +0100

    Replace mkDerivation with trivial builder.

--- default.nix
+++ default.nix
@@ -1,25 +1,14 @@
-{pkgs ? import <nixpkgs> {}}:
+{pkgs ? import <nixpkgs> {}, jre ? pkgs.jre }:
 
-pkgs.stdenv.mkDerivation {
+let
 
   name = "SlowMp3";
-  version = "0.1";
+
   src = pkgs.fetchurl {
     url = "http://www.rinki.net/pekka/slowmp3/SlowMP3.jar";
     sha256 = "1rcnlcsga7ifmnf93ldvb3jdj2vixj58c8sfx5vqpxg8xh3cyfl1";
   };
-  buildInputs = [ pkgs.jdk ];
 
-  phases = [ "installPhase" ];
-  
-  installPhase = ''
-    mkdir -p $out/bin
-    cd $out/bin
-    touch $name
-    chmod +x $name
-    cat <<EOF > $name
-      #!/usr/bin/env bash
-      java -jar $src
-    EOF
-  '';
-}
+  runCmd = "${jre}/bin/java -jar ${src}";
+
+in pkgs.writeShellScriptBin name runCmd
